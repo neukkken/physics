65ca4627fb3f3e29c104e2e4df622aeb81ff3f82
neukkken <neukken84@gmail.com>
2023-06-12 21:27:38 -0500

    Funcion de seno que no entiendo

--- funcionesDeTriangulos.py
+++ funcionesDeTriangulos.py
@@ -1,23 +1,22 @@
-def hiputenusa(base, altura):
+def calcular_seno(angulo, num_terminos):
+    angulo_rad = angulo * (3.14159265358979323846 / 180)  # Convierte el ángulo a radianes
+    seno = 0
 
+    for n in range(num_terminos):
+        termino = ((-1) ** n) * (angulo_rad ** (2 * n + 1)) / factorial(2 * n + 1)
+        seno += termino
 
-    h = base*base
-    h1 = altura*altura
+    return seno
 
-    hi = h*h + h1*h1
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
-    return hi
+angulo = 30  # Ángulo en grados
+terminos = 50  # Número de términos en la serie de Taylor (mayor número, mayor precisión)
 
+seno = calcular_seno(angulo, terminos)
 
-def opuesCat (num1, num2):
-
-    h = num1*num1
-    h1 = num2*num2
-    suma = h+h1
-    
-    res = raizC(suma)
-
-    return res
-
-a = hiputenusa(4, 5)
-print("A ",a)
+print("El seno de", angulo, "grados es:", seno)
